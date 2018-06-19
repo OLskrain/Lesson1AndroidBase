74b8841841bf0a9023d948811dc92c19c95680ab
OLskrain <34317856+OLskrain@users.noreply.github.com>
2018-06-19 23:39:58 +0300

    add test

--- app/src/test/java/com/olskrain/helloactivityhm/ExampleUnitTest.java
+++ app/src/test/java/com/olskrain/helloactivityhm/ExampleUnitTest.java
@@ -1,17 +1,52 @@
 package com.olskrain.helloactivityhm;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+
+    GreetingStrings greetingStrings = new GreetingStrings() {
+        @Override
+        public String getHelloer() { return "GeekBrains"; }
+        @Override
+        public String getMorning() { return "Good morning"; }
+        @Override
+        public String getAfternoon() { return "Good afternoon"; }
+        @Override
+        public String getEvening() { return "Good evening"; }
+        @Override
+        public String getNight() { return "Good night"; }
+    };
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void BuilderGreetingPhrase_Morning_Test() throws Exception {
+        BuilderGreetingPhrase builderGreetingPhrase = new BuilderGreetingPhrase(greetingStrings, 9);
+        String expected = "Good morning Andrey!";
+        String actual = builderGreetingPhrase.get();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void BuilderGreetingPhrase_Afternoon_Test() throws Exception {
+        BuilderGreetingPhrase builderGreetingPhrase = new BuilderGreetingPhrase(greetingStrings, 12);
+        String expected = "Good afternoon Andrey!";
+        String actual = builderGreetingPhrase.get();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void BuilderGreetingPhrase_Evening_Test() throws Exception {
+        BuilderGreetingPhrase builderGreetingPhrase = new BuilderGreetingPhrase(greetingStrings, 18);
+        String expected = "Good evening Andrey!";
+        String actual = builderGreetingPhrase.get();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void BuilderGreetingPhrase_Night_Test() throws Exception {
+        BuilderGreetingPhrase builderGreetingPhrase = new BuilderGreetingPhrase(greetingStrings, 22);
+        String expected = "Good night Andrey!";
+        String actual = builderGreetingPhrase.get();
+        assertEquals(expected, actual);
     }
 }
